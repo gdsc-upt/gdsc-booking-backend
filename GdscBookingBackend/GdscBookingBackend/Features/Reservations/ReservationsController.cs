f5b321c5893c666ea039650e4953f70aec337c96
Andrei Dobre <andrei.dobr3@gmail.com>
2023-11-25 17:34:33 +0200

    Added Endpoints for Reservations Controller

--- GdscBookingBackend/GdscBookingBackend/Features/Reservations/ReservationsController.cs
+++ GdscBookingBackend/GdscBookingBackend/Features/Reservations/ReservationsController.cs
@@ -1,4 +1,6 @@
+using GdscBookingBackend.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GdscBookingBackend.Features.Reservations;
 
@@ -6,50 +8,134 @@ namespace GdscBookingBackend.Features.Reservations;
 [Route("api/reservations")]
 public class ReservationsController : ControllerBase
 {
-    // private readonly IReservationsService _service;
-    //
-    // public ReservationsController(IReservationsService service)
-    // {
-    //     _service = service;
-    // }
-    //
-    // [HttpGet]
-    // public async Task<IActionResult> Get()
-    // {
-    //     var result = await _service.Get();
-    //
-    //     return Ok(result);
-    // }
-    //
-    // [HttpGet("{id}")]
-    // public async Task<IActionResult> Get(string id)
-    // {
-    //     var result = await _service.Get(id);
-    //
-    //     return Ok(result);
-    // }
-    //
-    // [HttpPost]
-    // public async Task<IActionResult> Post(Reservation model)
-    // {
-    //     var result = await _service.Create(model);
-    //
-    //     return Ok(result);
-    // }
-    //
-    // [HttpPut("{id}")]
-    // public async Task<IActionResult> Put(string id, Reservation model)
-    // {
-    //     var result = await _service.Update(id, model);
-    //
-    //     return Ok(result);
-    // }
-    //
-    // [HttpDelete("{id}")]
-    // public async Task<IActionResult> Delete(string id)
-    // {
-    //     var result = await _service.Delete(id);
-    //
-    //     return Ok(result);
-    // }
+    private readonly AppDbContext _dbContext;
+    
+    public ReservationsController(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult<ReservationModel>> Post(ReservationRequest request)
+    {
+       var model = new ReservationModel
+            {
+                Id = Guid.NewGuid().ToString(),
+                Created = DateTime.UtcNow,
+                Date = request.Date,
+                Interval = request.Interval,
+                UserId = request.UserId
+            };
+        
+            var result = await _dbContext.Reservations.AddAsync(model);
+            await _dbContext.SaveChangesAsync();
+            var uri = "/reservations/" + result.Entity.Id;
+        
+            return Created(uri, result.Entity);
+    }
+    
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<ActionResult<ReservationView>> Get()
+    {
+        return Ok(await _dbContext
+            .Reservations
+            .Select(model => new ReservationView
+            {
+                Id = model.Id,
+                Created = model.Created,
+                Date = model.Date,
+                Interval = model.Interval,
+                UserId = model.UserId
+            })
+            .ToListAsync()
+        );
+    }
+    
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<ReservationView>> Get(string id)
+    {
+        var result = await _dbContext.Reservations.FirstOrDefaultAsync(e => e.Id == id);
+        
+        if(result == null)
+        {
+            return NotFound($"Reservation with id {id} not found.");
+        }
+    
+        return Ok(new ReservationView
+        {
+            Id = result.Id,
+            Created = result.Created,
+            Date = result.Date,
+            Interval = result.Interval,
+            UserId = result.UserId
+        });
+    }
+    
+    [HttpPatch("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult<ReservationView>> Put(string id, ReservationRequest request)
+    {
+        var oldModel = await _dbContext.Reservations.FirstOrDefaultAsync(e => e.Id == id);
+        if (oldModel is null)
+        {
+            return NotFound($"Reservation with id {id} not found.");
+        }
+        
+        var newModel = new ReservationModel
+        {
+            Id = oldModel.Id,
+            Created = oldModel.Created,
+            Date = request.Date == new DateOnly() ? oldModel.Date : request.Date,
+            Interval = request.Interval == 0 ? oldModel.Interval : request.Interval,
+            UserId = request.UserId.Equals(string.Empty) ? oldModel.UserId : request.UserId
+        };
+        
+        var result = _dbContext.Reservations.Update(newModel);
+        await _dbContext.SaveChangesAsync();
+
+        return Ok(new ReservationView
+        {
+            Id = result.Entity.Id,
+            Created = result.Entity.Created,
+            Date = result.Entity.Date,
+            Interval = result.Entity.Interval,
+            UserId = result.Entity.UserId
+        });
+    }
+    
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult<ReservationView>> Delete(string id)
+    {
+        var model = await _dbContext.Reservations.FirstOrDefaultAsync(e => e.Id == id);
+        if (model is null)
+        {
+            return NotFound($"Reservation with id {id} not found.");
+        }
+        
+        var result = _dbContext.Reservations.Remove(model);
+        await _dbContext.SaveChangesAsync();
+    
+        return Ok(new ReservationView
+        {
+            Id = result.Entity.Id,
+            Created = result.Entity.Created,
+            Date = result.Entity.Date,
+            Interval = result.Entity.Interval,
+            UserId = result.Entity.UserId
+        });
+    }
 }
